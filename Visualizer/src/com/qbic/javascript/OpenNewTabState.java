ce6fbb2e079c1b527f33f205d716434a828b1165
David Wojnar <david.wojnar@uni-tuebingen.de>
2014-04-14 13:20:10 +0000

    restructured svn repository; partially legacy code removed
    git-svn-id: svn+ssh://sshgw-bs.informatik.uni-tuebingen.de/abi-projects/QBiC/PortalDevelopment/svn/trunk@52 a0440ff8-2b04-4d88-a457-8e570ff6c9eb

--- Visualizer/src/com/qbic/javascript/OpenNewTabState.java
+++
@@ -1,7 +0,0 @@
-package com.qbic.javascript;
-import com.vaadin.shared.ui.JavaScriptComponentState;
-
-@SuppressWarnings("serial")
-public class OpenNewTabState extends JavaScriptComponentState {
-    public String url;
-}
--- Visualizer/src/com/qbic/javascript/js_opennewtab.js
+++
@@ -1,8 +0,0 @@
-org_vaadin_blog_JsLabel = function() {
-	var e = this.getElement();
-	
-	this.onStateChange = function() {
-		var win=window.open(this.getState().url, '_blank');
-		  win.focus();
-	}
-}
